cca74f32ce34af6aacfa67ae28bd5ce20dd687f4
byIbrahimAli <59145840+byIbrahimAli@users.noreply.github.com>
2024-06-15 21:29:40 +0100

    feat: tada validation has been birthed

---
+++ frontend/errorMessages.js
@@ -0,0 +1,13 @@
+/**
+ * @summary the home for all client facing error messages 👍😎
+ */
+
+const errorMessages = {
+  REQUIRED: 'This field is required',
+  PASSWORD_TOO_SHORT:
+    "Password is too short 🤏, it's gotta be at least 8 characters long",
+  PASSWORD_TOO_WEAK:
+    'Woah, really want to make it easy for hackers? Add at least one uppercase letter, one lowercase letter, and one number',
+}
+
+export default errorMessages
---
+++ frontend/validationRules.js
@@ -0,0 +1,13 @@
+/**
+ * @summary all client-side validation rules can be found here 🤘
+ */
+
+const validationRules = {
+  email: {},
+  password: {},
+  name: {},
+  productId: {},
+  quantity: {},
+}
+
+export default validationRules
