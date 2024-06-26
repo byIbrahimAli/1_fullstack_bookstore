7f6bf260ca1a2f0df9444dbcec14ea4e25c50dd0
byIbrahimAli <59145840+byIbrahimAli@users.noreply.github.com>
2024-06-26 21:20:46 +0100

    test: basic validation tests added

--- frontend/validation.test.js
+++ frontend/validation.test.js
@@ -1,9 +1,34 @@
 import validateField from './validation'
 
-test('tests name cannot be a bunch of numbers', () => {
-  expect(validateField('name', '123456')).toBe(
-    `Name cannot contain numbers or special characters. Spaces, hyphens -, and apostrophes ', are allowed.`
-  )
-})
+describe('validation msg shown on regex mismatch', () => {
+  test('email field', () => {
+    expect(validateField('email', 'dot.dot.com')).toBe(
+      `Enter a valid email address`
+    )
+  })
 
-// todo: test your validation rules work through your validation fn
+  test('password field', () => {
+    expect(validateField('password', 'pass1234')).toBe(
+      `Password must be at least 8 characters long and contain an uppercase letter, a lowercase letter, and a number.`
+    )
+  })
+
+  test('name field', () => {
+    expect(validateField('name', '123456')).toBe(
+      `Name cannot contain numbers or special characters. Spaces, hyphens -, and apostrophes ', are allowed.`
+    )
+  })
+
+  test('productId field', () => {
+    expect(validateField('productId', '1Two,Three')).toBe(
+      `Product ID must be an alphanumeric value from 3 to 10 characters.`
+    )
+  })
+
+  test('quantity field', () => {
+    expect(validateField('quantity', '0')).toBe(
+      `Quantity has got to be bigger than 0.`
+    )
+  })
+  // hmm: validation is too simple, what'd be cool is precise error msgs on what the user needs to fix - i.e. add a capital letter; rather than the generic blanket error msg.
+})
